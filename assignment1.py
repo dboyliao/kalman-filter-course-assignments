20bf05cdd7ee8b1a7d9711f1e8a1ab714dabe2c3
dboyliao <qmalliao@gmail.com>
2021-04-01 16:25:04 +0800

    assignment1

--- assignment1.py
+++ assignment1.py
@@ -3,19 +3,26 @@ from sim.sim1d import sim_run
 
 # Simulator options.
 options = {}
-options['FIG_SIZE'] = [8,8]
-options['CONSTANT_SPEED'] = True
+options["FIG_SIZE"] = [8, 8]
+options["CONSTANT_SPEED"] = False
+
 
 class KalmanFilterToy:
-    def __init__(self):
+    def __init__(self, gain_ratio=0.5):
         self.v = 0
         self.prev_x = 0
         self.prev_t = 0
-    def predict(self,t):
-        prediction = 0
+        self.gain = gain_ratio
+
+    def predict(self, t):
+        prediction = self.prev_x + self.v * (t - self.prev_t)
         return prediction
-    def measure_and_update(self,x,t):
-        return
+
+    def measure_and_update(self, x, t):
+        measured_v = (x - self.prev_x) / (t - self.prev_t)
+        self.v += self.gain * (measured_v - self.v)
+        self.prev_x = x
+        self.prev_t = t
 
 
-sim_run(options,KalmanFilterToy)
+sim_run(options, KalmanFilterToy, gain_ratio=0.7)
